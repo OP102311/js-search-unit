6b56bb7708107ee4ae2a62fbf435878c5d3f957c
Shadomi <koibas.vis@gmail.com>
2016-02-13 02:08:59 +0300

    JSD-000 Реализована базовая логика поиска

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+var originalPage;
+
+var searchUnit;
+
+/**
+ * Initialization of searchUnit
+ */
+window.onload = function() {
+    originalPage = document.getElementById("content").innerHTML;
+    searchUnit = createInterface();
+    document.body.appendChild(searchUnit);
+}
+/**
+ * Shows or hides the search unit by pressing Ctrl+Alt+F
+ */
+document.addEventListener("keydown", function(event) {
+    if ((event.ctrlKey) && (event.altKey) && (event.keyCode == 70)) {
+        if (searchUnit!=null) {
+            searchUnit.style.display = (searchUnit.style.display == 'none') ? '' : 'none';
+            document.getElementById('search-input').focus();
+        }
+    }
+});
+
 /**
  * Creates a search unit interface
  * @returns {HTMLElement}
@@ -9,23 +33,32 @@ function createInterface() {
     var searchInput = document.createElement('input');
     searchInput.id = "search-input";
     searchInput.type = 'text';
+    searchInput.setAttribute('onkeyup','searchText()');
+
     var searchCheckbox = document.createElement('input');
     searchCheckbox.type = 'checkbox';
     createCheckbox('match-case', 'match case');
+
     var searchNextButton = document.createElement('input');
     searchNextButton.type = 'button';
     searchNextButton.value = 'Next';
+
     var searchPreviousButton = document.createElement('input');
     searchPreviousButton.type = 'button';
     searchPreviousButton.value = 'Previous';
 
-    document.body.appendChild(searchUnit);
+    var closeButton = document.createElement('input');
+    closeButton.type = 'button';
+    closeButton.value = 'Close';
+
     searchUnit.appendChild(searchInput);
     searchUnit.appendChild(createCheckbox('match-case', 'match case'));
     searchUnit.appendChild(createCheckbox('whole-word', 'whole word'));
     searchUnit.appendChild(createCheckbox('regular-exp', 'regular expression'));
     searchUnit.appendChild(searchNextButton);
     searchUnit.appendChild(searchPreviousButton);
+    searchUnit.appendChild(closeButton);
+
     return searchUnit;
 }
 
@@ -46,16 +79,32 @@ function createCheckbox(name, title) {
 }
 
 /**
- * Shows or hides the search unit by pressing Ctrl+Alt+F
+ * Finds and highlights the search result
  */
-document.addEventListener("keydown", function(event) {
-    if ((event.ctrlKey) && (event.altKey) && (event.keyCode == 70)) {
-        var searchUnit = document.getElementById('search-unit');
-        if (searchUnit!=null) {
-            searchUnit.style.display = (searchUnit.style.display == 'none') ? '' : 'none';
-        } else {
-            createInterface();
+function searchText() {
+    var element = document.getElementById("content");
+    element = restorePage(element);
+    var tempinnerHTML = element.innerHTML;
+
+    var searchString = document.getElementById('search-input').value;
+
+    if (searchString!='') {
+        element.innerHTML = tempinnerHTML.replace(/>([^<]*)?([^>]*)?</gi, replacer);
+        function replacer(str) {
+            var regex = new RegExp(searchString, "g");
+            var result = str.replace(regex, '<span style="background:#FFFF00;">'+searchString+'</span>');
+            return result;
         }
-        document.getElementById('search-input').focus();
     }
-});
+}
+
+/**
+ * Restores page from the source
+ * @param element
+ * @returns {*}
+ */
+function restorePage(element)
+{
+    element.innerHTML = originalPage;
+    return element;
+}
